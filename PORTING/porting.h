db764aab6b941dee4421961f8c13c90b57e78b59
fred.li <fred.li@arm.com>
2019-11-25 11:35:27 +0800

    Mannual Copy SDK

---
+++ PORTING/porting.h
@@ -0,0 +1,100 @@
+/*Reference from ST SDK main.h merge a lot together.*/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __PORTING_H
+#define __PORTING_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_nucleo_144.h"
+#include "stdio.h"
+#include "cmsis_os.h"
+
+#include "ff_gen_drv.h"
+#include "usbh_diskio_dma.h"
+
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+
+/* User can use this section to tailor USARTx/UARTx instance used and associated
+   resources */
+/* Definition for USARTx clock resources */
+#define USARTx                           USART3
+#define USARTx_CLK_ENABLE()              __HAL_RCC_USART3_CLK_ENABLE();
+#define USARTx_RX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOD_CLK_ENABLE()
+#define USARTx_TX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOD_CLK_ENABLE()
+
+#define USARTx_FORCE_RESET()             __HAL_RCC_USART3_FORCE_RESET()
+#define USARTx_RELEASE_RESET()           __HAL_RCC_USART3_RELEASE_RESET()
+
+/* Definition for USARTx Pins */
+#define USARTx_TX_PIN                    GPIO_PIN_8
+#define USARTx_TX_GPIO_PORT              GPIOD
+#define USARTx_TX_AF                     GPIO_AF7_USART3
+#define USARTx_RX_PIN                    GPIO_PIN_9
+#define USARTx_RX_GPIO_PORT              GPIOD
+#define USARTx_RX_AF                     GPIO_AF7_USART3
+
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/ 
+#define USE_DHCP       /* enable DHCP, if disabled static address is used*/
+ 
+/*Static IP ADDRESS*/
+#define IP_ADDR0   (uint8_t) 192
+#define IP_ADDR1   (uint8_t) 168
+#define IP_ADDR2   (uint8_t) 0
+#define IP_ADDR3   (uint8_t) 10
+   
+/*NETMASK*/
+#define NETMASK_ADDR0   (uint8_t) 255
+#define NETMASK_ADDR1   (uint8_t) 255
+#define NETMASK_ADDR2   (uint8_t) 255
+#define NETMASK_ADDR3   (uint8_t) 0
+
+/*Gateway Address*/
+#define GW_ADDR0   (uint8_t) 192
+#define GW_ADDR1   (uint8_t) 168
+#define GW_ADDR2   (uint8_t) 0
+#define GW_ADDR3   (uint8_t) 1 
+
+/* Exported macro ------------------------------------------------------------*/
+
+/* Base address of the Flash sectors Bank 1 */
+#define ADDR_FLASH_SECTOR_0     ((uint32_t)0x08000000) /* Base @ of Sector 0, 16 Kbytes */
+#define ADDR_FLASH_SECTOR_1     ((uint32_t)0x08004000) /* Base @ of Sector 1, 16 Kbytes */
+#define ADDR_FLASH_SECTOR_2     ((uint32_t)0x08008000) /* Base @ of Sector 2, 16 Kbytes */
+#define ADDR_FLASH_SECTOR_3     ((uint32_t)0x0800C000) /* Base @ of Sector 3, 16 Kbytes */
+#define ADDR_FLASH_SECTOR_4     ((uint32_t)0x08010000) /* Base @ of Sector 4, 64 Kbytes */
+#define ADDR_FLASH_SECTOR_5     ((uint32_t)0x08020000) /* Base @ of Sector 5, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_6     ((uint32_t)0x08040000) /* Base @ of Sector 6, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_7     ((uint32_t)0x08060000) /* Base @ of Sector 7, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_8     ((uint32_t)0x08080000) /* Base @ of Sector 8, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_9     ((uint32_t)0x080A0000) /* Base @ of Sector 9, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_10    ((uint32_t)0x080C0000) /* Base @ of Sector 10, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_11    ((uint32_t)0x080E0000) /* Base @ of Sector 11, 128 Kbytes */
+
+/* Base address of the Flash sectors Bank 2 */
+#define ADDR_FLASH_SECTOR_12     ((uint32_t)0x08100000) /* Base @ of Sector 0, 16 Kbytes */
+#define ADDR_FLASH_SECTOR_13     ((uint32_t)0x08104000) /* Base @ of Sector 1, 16 Kbytes */
+#define ADDR_FLASH_SECTOR_14     ((uint32_t)0x08108000) /* Base @ of Sector 2, 16 Kbytes */
+#define ADDR_FLASH_SECTOR_15     ((uint32_t)0x0810C000) /* Base @ of Sector 3, 16 Kbytes */
+#define ADDR_FLASH_SECTOR_16     ((uint32_t)0x08110000) /* Base @ of Sector 4, 64 Kbytes */
+#define ADDR_FLASH_SECTOR_17     ((uint32_t)0x08120000) /* Base @ of Sector 5, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_18     ((uint32_t)0x08140000) /* Base @ of Sector 6, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_19     ((uint32_t)0x08160000) /* Base @ of Sector 7, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_20     ((uint32_t)0x08180000) /* Base @ of Sector 8, 128 Kbytes  */
+#define ADDR_FLASH_SECTOR_21     ((uint32_t)0x081A0000) /* Base @ of Sector 9, 128 Kbytes  */
+#define ADDR_FLASH_SECTOR_22     ((uint32_t)0x081C0000) /* Base @ of Sector 10, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_23     ((uint32_t)0x081E0000) /* Base @ of Sector 11, 128 Kbytes */
+
+/* Exported functions ------------------------------------------------------- */  
+
+
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+#endif /* __PORTING_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
--- README.md
+++ README.md
@@ -1,1 +1,117 @@
 # Example-PAL-Porting-STM32F429ZI-FreeRtos
+
+0. Switch to this README PATH.
+
+1, Copy from SDK: STM32Cube_FW_F4_V1.24.0\Drivers\* contents to .\Drivers folder. 
+
+2, Copy from SDK: STM32Cube_FW_F4_V1.24.0\Middlewares\Third_Party\FreeRTOS\* contents to .\OS\FreeRTOS_10.0.1_ST folder.
+
+3, Copy from SDK: 
+STM32Cube_FW_F4_V1.24.0\Middlewares\Third_Party\FatFs\* contents to .\Middleware\FatFs_R0.12C folder.
+
+4, Copy from SDK:
+STM32Cube_FW_F4_V1.24.0\Middlewares\Third_Party\LwIP\* contents to .\Middleware\LwIP_2.0.3 folder.
+
+5, Copy from SDK: 
+STM32Cube_FW_F4_V1.24.0\Middlewares\ST\STM32_USB_Host_Library\* contents to .\Middleware\STM32_USB_Host_Library folder.
+
+6, Final Folder Tree:
+
+.
+├── Drivers
+│   ├── BSP
+│   │   ├── Adafruit_Shield
+│   │   ├── Components
+│   │   ├── STM32412G-Discovery
+│   │   ├── STM32446E_EVAL
+│   │   ├── STM32469I-Discovery
+│   │   ├── STM32469I_EVAL
+│   │   ├── STM324x9I_EVAL
+│   │   ├── STM324xG_EVAL
+│   │   ├── STM32F401-Discovery
+│   │   ├── STM32F411E-Discovery
+│   │   ├── STM32F413H-Discovery
+│   │   ├── STM32F429I-Discovery
+│   │   ├── STM32F4-Discovery
+│   │   ├── STM32F4xx-Nucleo
+│   │   └── STM32F4xx_Nucleo_144
+│   ├── CMakeLists.txt
+│   ├── CMSIS
+│   │   ├── ARM.CMSIS.pdsc
+│   │   ├── Core
+│   │   ├── Core_A
+│   │   ├── Device
+│   │   ├── docs
+│   │   ├── DSP
+│   │   ├── Include
+│   │   ├── Lib
+│   │   ├── LICENSE.txt
+│   │   ├── NN
+│   │   ├── README.md
+│   │   ├── RTOS
+│   │   └── RTOS2
+│   ├── STM32F4xx_HAL_Driver
+│   │   ├── Inc
+│   │   ├── Release_Notes.html
+│   │   ├── Src
+│   │   ├── STM32F410Rx_User_Manual.chm
+│   │   ├── STM32F411xE_User_Manual.chm
+│   │   ├── STM32F412Zx_User_Manual.chm
+│   │   ├── STM32F417xx_User_Manual.chm
+│   │   ├── STM32F423xx_User_Manual.chm
+│   │   ├── STM32F439xx_User_Manual.chm
+│   │   ├── STM32F446xx_User_Manual.chm
+│   │   └── STM32F479xx_User_Manual.chm
+│   └── system_stm32f4xx.c
+├── Middleware
+│   ├── FatFs_R0.12C
+│   │   ├── CMakeLists.txt
+│   │   ├── doc
+│   │   └── src
+│   ├── LwIP_2.0.3
+│   │   ├── CHANGELOG
+│   │   ├── CMakeLists.txt
+│   │   ├── COPYING
+│   │   ├── doc
+│   │   ├── FILES
+│   │   ├── port
+│   │   ├── README
+│   │   ├── src
+│   │   ├── st_readme.txt
+│   │   ├── system
+│   │   ├── test
+│   │   └── UPGRADING
+│   └── STM32_USB_Host_Library
+│       ├── Class
+│       ├── CMakeLists.txt
+│       ├── Core
+│       └── Release_Notes.html
+├── OS
+│   └── FreeRTOS_10.0.1_ST
+│       ├── CMakeLists.txt
+│       ├── License
+│       ├── links_to_doc_pages_for_the_demo_projects.url
+│       ├── readme.txt
+│       └── Source
+├── PORTING
+│   ├── CMakeLists.txt
+│   ├── ffconf.h
+│   ├── FreeRTOSConfig.h
+│   ├── porting.h
+│   ├── stm32f4xx_hal_conf.h
+│   ├── stm32f4xx_hal_msp.c
+│   ├── stm32f4xx_hal_timebase_tim.c
+│   ├── stm32f4xx_it.c
+│   ├── stm32f4xx_it.h
+│   ├── TOOLCHAIN_ARM_STD
+│   │   ├── startup_stm32f429xx.S
+│   │   └── stm32f429xx.sct
+│   ├── TOOLCHAIN_GCC_ARM
+│   │   ├── STM32F429xI.ld
+│   │   └── syscalls.c
+│   ├── usbh_conf.c
+│   ├── usbh_conf.h
+│   ├── usbh_diskio_dma.c
+│   └── usbh_diskio_dma.h
+└── README.md
+
